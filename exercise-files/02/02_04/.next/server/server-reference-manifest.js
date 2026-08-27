d4b8f8e056aa06f52d7d872f0079f7d9d95b1677
phileb52 <every.phil@gmail.com>
2026-08-27 05:10:26 +0000

    ds

--- exercise-files/02/02_04/.next/server/server-reference-manifest.js
+++ exercise-files/02/02_04/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lji2kbMy8szGsBtf+zP9lRyb+5rQSzN3fvzrrFbJKyQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"otbKi3elj7VQ/69HufbyB8TX7Ta+kbQMW+C8ksPxhps=\"\n}"
--- exercise-files/02/02_04/.next/static/chunks/webpack.js
+++ exercise-files/02/02_04/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4d14fa416f396e6"; }
+/******/ 		__webpack_require__.h = function() { return "ec0af2b6b59998ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
